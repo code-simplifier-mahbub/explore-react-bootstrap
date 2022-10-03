ad57444a9acc8685576499958eca9dc6fc2748e1
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-10-03 12:15:05 +0600

    explore bootstrap in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Utilities from './component/Utilities/Utilities';
+import Accordian from './component/Accordian/Accordian';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Utilities></Utilities>
+      <Accordian></Accordian>
+      <Button>Gesire Help Kor Tora Kew</Button>
+      <Button>Tora kew Help korli na...</Button>
+      
     </div>
   );
 }
