12c6df324c4210ad20c82aadfbb560e1de6e9018
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-10-03 12:23:37 +0600

    explore bootstrap in react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Utilities from './component/Utilities/Utilities';
 import Accordian from './component/Accordian/Accordian';
 import { Button } from 'react-bootstrap';
+import Loading from './component/Loading/Loading';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Accordian></Accordian>
       <Button>Gesire Help Kor Tora Kew</Button>
       <Button>Tora kew Help korli na...</Button>
-      
+      <Loading></Loading>
     </div>
   );
 }
